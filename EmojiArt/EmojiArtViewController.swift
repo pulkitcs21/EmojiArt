b5372d2e0f01f232eca7e26fd4405ae13a825b05
Pulkit Agarwal <pulkit@Pulkits-MacBook-Pro.local>
2018-04-21 21:27:48 -0700

    Drag and Drop Implemented

--- EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArtViewController.swift
@@ -25,11 +25,21 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate{
         return UIDropProposal(operation: .copy)
     }
     
+    var imageFetcher: ImageFetcher!
+    
     func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        imageFetcher = ImageFetcher () {(url, image) in
+            self.emojiArtView.backgroundImage = image
+        }
         session.loadObjects(ofClass: NSURL.self) { nsurl in
-            if let
+            if let url = nsurl.first as? URL{
+                self.imageFetcher.fetch(url)
+                }
             }
-        session.loadObjects(ofClass: UIImage.self { images in
+        session.loadObjects(ofClass: UIImage.self) { images in
+            if let image = images.first as? UIImage{
+                self.imageFetcher.backup = image
+            }
         }
     }
     @IBOutlet weak var emojiArtView: EmojiArtView!
